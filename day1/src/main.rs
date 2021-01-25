5709368e7bbfbcd7caebbb67b1bd848a1e98b038
Warren Kretzschmar <warren.kretzschmar@ki.se>
2021-01-25 21:56:13 +0100

    Code cleanup

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -8,15 +8,17 @@ fn main() {
 
     let contents = fs::read_to_string(filename)
         .expect("Something went wrong reading the file");
-    let nums: Vec<_> = contents.split('\n').filter_map(|s| s.parse::<u32>().ok()).collect();
-    for (idx, i) in nums.iter().enumerate(){
-        for j in nums[idx+1..].iter() {
-            println!("{}, {}: {}", idx, i, j);
+    let nums: Vec<_> = contents
+        .split('\n')
+        .filter_map(|s| s.parse::<u32>().ok())
+        .collect();
+    for (idx, i) in nums.iter().enumerate() {
+        for j in nums[idx + 1..].iter() {
             if i + j == 2020 {
+                println!("{}, {}: {}", idx, i, j);
                 println!("Result is {}", i * j);
                 process::exit(0);
             }
-
         }
     }
 }
