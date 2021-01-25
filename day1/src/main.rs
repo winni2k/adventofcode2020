5e3aec5540dbd1e69d6ab6b048cfbb52d61f4fa2
Warren Kretzschmar <warren.kretzschmar@ki.se>
2021-01-25 21:49:48 +0100

    Day1a

---
+++ day1/src/main.rs
@@ -0,0 +1,22 @@
+use std::env;
+use std::fs;
+use std::process;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let filename = &args[1];
+
+    let contents = fs::read_to_string(filename)
+        .expect("Something went wrong reading the file");
+    let nums: Vec<_> = contents.split('\n').filter_map(|s| s.parse::<u32>().ok()).collect();
+    for (idx, i) in nums.iter().enumerate(){
+        for j in nums[idx+1..].iter() {
+            println!("{}, {}: {}", idx, i, j);
+            if i + j == 2020 {
+                println!("Result is {}", i * j);
+                process::exit(0);
+            }
+
+        }
+    }
+}
