2a88019a16ab518cc32e133bdec8e13d9a6e2c7c
Warren Kretzschmar <warren.kretzschmar@ki.se>
2021-02-01 14:17:54 +0100

    Refactor to use ? operator

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,18 +1,19 @@
 use regex::Regex;
 use std::env;
 use std::fs;
+use std::error::Error;
 
-fn main() {
+fn main() -> Result<(), Box<dyn Error>> {
     let args: Vec<String> = env::args().collect();
     let filename = &args[1];
 
     let contents = fs::read_to_string(filename)
         .expect("Something went wrong reading the file");
-    let re = Regex::new(r"(\d+)-(\d+) ([a-z]): ([a-z]+)").unwrap();
+    let re = Regex::new(r"(\d+)-(\d+) ([a-z]): ([a-z]+)")?;
     let mut n_valid_pw: u32 = 0;
     for cap in re.captures_iter(&contents) {
-        let lower = cap[1].parse::<u32>().unwrap() as usize;
-        let upper = cap[2].parse::<u32>().unwrap() as usize;
+        let lower = cap[1].parse::<u32>()? as usize;
+        let upper = cap[2].parse::<u32>()? as usize;
 
         println!("{}-{} {}: {}", &cap[1], &cap[2], &cap[3], &cap[4]);
         assert!(lower <= upper);
@@ -25,4 +26,5 @@ fn main() {
         }
     }
     println!("n valid passwords: {}", n_valid_pw);
+    Ok(())
 }
--- day2b/src/main.rs
+++ day2b/src/main.rs
@@ -1,22 +1,27 @@
 use regex::Regex;
 use std::env;
 use std::fs;
+use std::error::Error;
 
-fn main() {
+fn main() -> Result<(), Box<dyn Error>> {
     let args: Vec<String> = env::args().collect();
     let filename = &args[1];
 
     let contents = fs::read_to_string(filename)
         .expect("Something went wrong reading the file");
-    let re = Regex::new(r"(\d+)-(\d+) ([a-z]): ([a-z]+)").unwrap();
+    let re = Regex::new(r"(\d+)-(\d+) ([a-z]): ([a-z]+)")?;
     let mut n_valid_pw: u32 = 0;
     for cap in re.captures_iter(&contents) {
-        let first = cap[1].parse::<u32>().unwrap() as usize;
-        let second = cap[2].parse::<u32>().unwrap() as usize;
-        if (cap[4][first-1..first] == cap[3]) != (cap[4][second-1..second] == cap[3]){
+        let first = cap[1].parse::<u32>()? as usize;
+        let second = cap[2].parse::<u32>()? as usize;
+        let password: Vec<char> = cap[4].chars().collect();
+        let letter: char = cap[3].chars().next().unwrap();
+        if (password[first - 1] == letter)
+            != (password[second - 1] == letter) {
             println!("{}-{} {}: {}", &cap[1], &cap[2], &cap[3], &cap[4]);
             n_valid_pw += 1;
         }
     }
     println!("n valid passwords: {}", n_valid_pw);
+    Ok(())
 }
