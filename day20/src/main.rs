861a8872bff972806dcf8e7b06bd56755c376fe5
Warren Kretzschmar <winni@warrenwk.com>
2023-08-20 22:14:03 +0200

    More changes

--- day20/src/main.rs
+++ day20/src/main.rs
@@ -1,14 +1,18 @@
 use combinations::Combinations;
-use ndarray::{s, Array2, ArrayView1};
+//use ndarray::{s, Array2, ArrayView, ArrayBase};
+use ndarray::prelude::*;
 use regex::Regex;
 use std::collections::{HashMap, HashSet};
 use std::fs::read_to_string;
 
 fn main() -> std::io::Result<()> {
-    //    let s = read_to_string("test.txt")?;
-    let s = read_to_string("adventofcode.com_2020_day_20_input.txt")?;
+    let s = read_to_string("test.txt")?;
+    // let s = read_to_string("adventofcode.com_2020_day_20_input.txt")?;
 
-    let tile_objects = s.trim_end_matches('\n').split("\n\n").map(Tile::new);
+    let tile_objects = s
+        .trim_end_matches('\n')
+        .split("\n\n")
+        .map(Tile::from_string);
     let mut tiles = HashMap::new();
     for tile in tile_objects {
         tiles.insert(tile.id, tile);
@@ -29,7 +33,6 @@ fn main() -> std::io::Result<()> {
             });
         }
     }
-    println!("{:?}", tiles);
     let mut corner_ids: Vec<_> = vec![];
     for (key, val) in tiles.iter() {
         if val.neighbors.len() == 2 {
@@ -48,16 +51,42 @@ fn main() -> std::io::Result<()> {
     Ok(())
 }
 
+const NROW: usize = 10;
+
 #[derive(Debug)]
 struct Tile {
     id: u32,
     data: ndarray::Array2<u8>,
     neighbors: HashSet<u32>,
-    edges: Vec<u16>,
+    edges_smaller: Vec<u16>,
+}
+
+fn edges_from_arr2(data: &Array2<u8>) -> Vec<u16> {
+    let slices = vec![
+        data.slice(s![0, ..]),
+        data.slice(s![.., -1]),
+        data.slice(s![-1, ..]),
+        data.slice(s![.., 0]),
+    ];
+    let mut edges_smaller = vec![0u16, 0, 0, 0];
+    for (edge_idx, slice) in slices.iter().enumerate() {
+        let mut dat_int: u16 = 0;
+        let mut dat_int2: u16 = 0;
+        for (i, val) in slice.iter().enumerate() {
+            dat_int |= (*val as u16) << i;
+            dat_int2 |= (*val as u16) << (NROW - i - 1);
+        }
+        let dat_val_smaller = match dat_int < dat_int2 {
+            true => dat_int,
+            false => dat_int2,
+        };
+        edges_smaller[edge_idx] = dat_val_smaller;
+    }
+    edges_smaller
 }
 
 impl Tile {
-    fn new(tile_text: &str) -> Tile {
+    fn from_string(tile_text: &str) -> Tile {
         let num_re = Regex::new(r"^Tile (\d+)").unwrap();
         let mut lines = tile_text.split('\n');
         let line = lines.next().unwrap();
@@ -70,36 +99,23 @@ impl Tile {
                 dat.push((char == '#') as u8);
             }
         }
-        const NROW: usize = 10;
         let data = Array2::from_shape_vec((NROW, NROW), dat).unwrap();
-        println!("{:?}", data);
-        let slices = vec![
-            data.slice(s![0, ..]),
-            data.slice(s![-1, ..]),
-            data.slice(s![.., 0]),
-            data.slice(s![.., 9]),
-        ];
-        let mut edges = vec![0u16, 0, 0, 0];
-        for (edge_idx, slice) in slices.iter().enumerate() {
-            let mut dat_int: u16 = 0;
-            let mut dat_int2: u16 = 0;
-            for (i, val) in slice.iter().enumerate() {
-                dat_int |= (*val as u16) << i;
-                dat_int2 |= (*val as u16) << (NROW - i - 1);
-            }
-            let dat_val = match dat_int < dat_int2 {
-                true => dat_int,
-                false => dat_int2,
-            };
-            edges[edge_idx] = dat_val;
-        }
 
+        Self::from_arr2(id, data)
+    }
+
+    fn from_arr2(id: u32, data: Array2<u8>) -> Tile {
+        let edges_smaller = edges_from_arr2(&data);
         Tile {
             id,
             data,
             neighbors: HashSet::new(),
-            edges,
+            edges_smaller,
         }
+    }
+    fn edges(&self, idx: usize) -> ArrayView1<u8> {
+        let slices = vec![s![0, ..], s![.., -1], s![-1, ..], s![.., 0]];
+        self.data.slice(slices[idx])
     }
     fn is_neighbor(&self, other: &Tile) -> bool {
         match self.find_neighbor_edges(other) {
@@ -108,13 +124,56 @@ impl Tile {
         }
     }
     fn find_neighbor_edges(&self, other: &Tile) -> Option<(usize, usize)> {
-        for (i, ev1) in self.edges.iter().enumerate() {
-            for (j, ev2) in other.edges.iter().enumerate() {
+        for (i, ev1) in self.edges_smaller.iter().enumerate() {
+            for (j, ev2) in other.edges_smaller.iter().enumerate() {
                 if ev1 == ev2 {
                     return Some((i, j));
                 }
             }
         }
         None
+    }
+    fn flip_and_rotate_to_match(&mut self, other: &Tile) {
+        let (eidx_self, eidx_other) = self.find_neighbor_edges(other).unwrap();
+        if self.edges(eidx_self) != other.edges(eidx_other) {
+            self.flip()
+        }
+        //let (eidx_self, eidx_other) = self.find_neighbor_edges(other).unwrap();
+    }
+    fn flip(&mut self) {
+        self.edges_smaller.swap(1, 3);
+        let data_flip = self.data.slice_mut(s![.., ..;-1]).to_owned();
+        self.data = data_flip;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    // Note this useful idiom: importing names from outer (for mod tests) scope.
+    use super::*;
+    use ndarray::{arr2, Array};
+
+    #[test]
+    fn test_flip() {
+        // given
+        let mut tile = Tile::from_arr2(0, Array::eye(10));
+        // when
+        tile.flip();
+        // then
+        assert_eq!(
+            tile.data,
+            arr2(&[
+                [0u8, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+                [0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+                [0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+                [0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
+                [0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+                [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+                [0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+                [0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+                [0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+                [1, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+            ])
+        );
     }
 }
