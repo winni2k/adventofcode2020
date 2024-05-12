449647d6b14d7c77e6fd8a8f49623aa7916942c7
W Winfried Kretzschmar <winni@warrenwk.com>
2024-05-12 21:58:49 +0200

    day8b

--- day8/src/main.rs
+++ day8/src/main.rs
@@ -1,12 +1,17 @@
 use std::{fs, iter::zip};
 
+#[derive(Debug)]
+enum MyError {
+    InfiniteLoop(),
+}
+
 
 fn main() {
     let path = "./input.txt";
     let contents = fs::read_to_string(path).unwrap();
 
-    let mut command = vec!();
-    let mut offset: Vec<i32> = vec!();
+    let mut command = vec![];
+    let mut offset: Vec<i32> = vec![];
     for line in contents.lines() {
         let words: Vec<_> = line.split(' ').collect();
         command.push(words[0]);
@@ -16,22 +21,43 @@ fn main() {
         println!("{} {}", com, off);
     }
 
+    for i in 0..command.len(){
+        let old_op = command[i];
+        match old_op {
+            "nop" => command[i] = "jmp" ,
+            "jmp" => command[i] = "nop",
+            "acc" => continue,
+            _ => panic!("Unexpected operator"),
+
+        }
+        if let Ok(acc) = run_code(&command, &offset) {
+            println!("Successful run. acc = {}", acc);
+            break;
+        }
+        command[i] = old_op;
+    } 
+}
+
+fn run_code(command: &[&str], offset: &[i32]) -> Result<i32, MyError>{
     let mut acc: i32 = 0;
     let mut i = 0;
-    let mut seen = vec!();
+    let mut seen = vec![];
     while i != command.len() {
         if seen.contains(&i) {
-            panic!("Found instruction run a second time. i = {}, acc = {}", i, acc);
+            return Err(
+                MyError::InfiniteLoop(
+            ));
         }
         seen.push(i);
         match command[i] {
             "acc" => {
                 acc += offset[i];
                 i += 1;
-            },
+            }
             "nop" => i += 1,
             "jmp" => i = (i as i32 + offset[i]) as usize,
             _ => panic!("Unexpected operator"),
         }
     }
+    Ok(acc)
 }
