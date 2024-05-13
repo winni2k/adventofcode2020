9211c16c29b1d6cce206035e7d13f3cfd81578a5
W Winfried Kretzschmar <winni@warrenwk.com>
2024-05-13 15:39:42 +0200

    day10b -- WIP

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -1,7 +1,7 @@
 use std::{collections::HashMap, fs, iter::zip};
 
 fn main() {
-    let path = "./input.txt";
+    let path = "./test.txt";
     let contents = fs::read_to_string(path).unwrap();
 
     let mut numbers: Vec<_> = contents
@@ -24,6 +24,39 @@ fn main() {
     for jolt in jolts {
         *counts.entry(jolt).or_insert(0) += 1;
     }
-    dbg!(&counts);
-    println!("Result: {}", counts[&1] * counts[&3])
+    // dbg!(&counts);
+    println!("Result: {}", counts[&1] * counts[&3]);
+
+    let mut unseen: Vec<Vec<usize>> = vec![vec![]];
+    let mut seen: Vec<Vec<usize>> = vec![];
+    // dbg!(&numbers);
+    // dbg!(&unseen);
+    let mut dbg_count = 0;
+    while !unseen.is_empty() {
+        if let Some(arr) = unseen.pop() {
+            let first = *arr.last().unwrap_or(&1) + 1;
+            for i in first..(numbers.len() - 1) {
+                // dbg!(&i);
+                let mut prev_idx = i - 1;
+                while arr.contains(&prev_idx) {
+                    prev_idx -= 1;
+                }
+                let mut next_idx = i + 1;
+                while arr.contains(&next_idx) {
+                    next_idx += 1;
+                }
+                if numbers[next_idx] - numbers[prev_idx] < 4 {
+                    let mut candidate = arr.clone();
+                    candidate.push(i);
+                    unseen.push(candidate);
+                }
+            }
+            seen.push(arr);
+        }
+        // dbg!(&seen);
+        // dbg!(&unseen);
+    }
+    // dbg!(&seen);
+    // dbg!(&unseen);
+    println!("Number of charging arrangements: {}", seen.len());
 }
